bd4a9043d67b3ede5db0734c52cb22766e953bed
Fabian Pichler <fabian.pichler@synventis.com>
2025-08-28 15:05:31 +0200

    add missing import

--- celements-user-manager/component/src/main/java/com/celements/auth/AuthenticationScriptService.java
+++ celements-user-manager/component/src/main/java/com/celements/auth/AuthenticationScriptService.java
@@ -6,6 +6,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xwiki.component.annotation.Component;
