fbe0c0e17ddf0718a2889482a2712b418a833089
Marc Sladek <marc@sladek.dev>
2026-07-02 23:13:34 +0200

    XWikiAdminUser mandatory (#91)

---
+++ celements-user-manager/component/src/main/java/com/celements/mandatory/XWikiAdminUser.java
@@ -0,0 +1,95 @@
+package com.celements.mandatory;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.xwiki.component.annotation.Requirement;
+import org.xwiki.model.reference.DocumentReference;
+
+import com.celements.model.classes.ClassDefinition;
+import com.celements.model.object.xwiki.XWikiObjectEditor;
+import com.celements.model.reference.RefBuilder;
+import com.celements.web.classes.oldcore.XWikiGroupsClass;
+import com.xpn.xwiki.XWikiConstant;
+import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.doc.XWikiDocument;
+
+@Component("celements.mandatory.XWikiAdminUser")
+public class XWikiAdminUser extends AbstractMandatoryDocument {
+
+  public static final String ADMIN_DOC_NAME = "Admin";
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(XWikiAdminUser.class);
+
+  @Requirement(XWikiGroupsClass.CLASS_DEF_HINT)
+  private ClassDefinition groupsClass;
+
+  @Override
+  public String getName() {
+    return "XWikiAdminUser";
+  }
+
+  @Override
+  public List<String> dependsOnMandatoryDocuments() {
+    return List.of(
+        "celements.MandatoryGroups", // Admin group must exist
+        MandatoryDiskDocuments.class.getName()); // Admin user must exist, XWiki/Admin.xml
+  }
+
+  @Override
+  protected DocumentReference getDocRef() {
+    return new RefBuilder().with(modelContext.getWikiRef())
+        .space(XWikiConstant.XWIKI_SPACE)
+        .doc("XWikiAdminGroup")
+        .build(DocumentReference.class);
+  }
+
+  @Override
+  protected boolean isEnabledByDefault() {
+    return false;
+  }
+
+  @Override
+  protected boolean skip() {
+    return !modelUtils.isMainWiki(modelContext.getWikiRef());
+  }
+
+  @Override
+  protected boolean checkDocuments(XWikiDocument doc) throws XWikiException {
+    return false;
+  }
+
+  @Override
+  protected boolean checkDocumentsMain(XWikiDocument doc) throws XWikiException {
+    return addAdminUserToAdminGroup(doc);
+  }
+
+  boolean addAdminUserToAdminGroup(XWikiDocument groupDoc) {
+    var adminUserDocRef = getAdminUserDocRef();
+    var editor = XWikiObjectEditor.on(groupDoc)
+        .filter(groupsClass)
+        .filter(XWikiGroupsClass.FIELD_MEMBER, adminUserDocRef);
+    if (editor.fetch().exists()) {
+      return false;
+    }
+    editor.createFirst();
+    LOGGER.info("added [{}] to [{}]", adminUserDocRef, groupDoc.getDocRef());
+    return true;
+  }
+
+  private DocumentReference getAdminUserDocRef() {
+    return new RefBuilder()
+        .with(modelContext.getWikiRef())
+        .space(XWikiConstant.XWIKI_SPACE)
+        .doc(ADMIN_DOC_NAME)
+        .build(DocumentReference.class);
+  }
+
+  @Override
+  public Logger getLogger() {
+    return LOGGER;
+  }
+
+}
---
+++ celements-user-manager/component/src/test/java/com/celements/mandatory/XWikiAdminUserTest.java
@@ -0,0 +1,128 @@
+package com.celements.mandatory;
+
+import static com.celements.common.test.CelementsTestUtils.*;
+import static org.easymock.EasyMock.*;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.xwiki.model.reference.DocumentReference;
+
+import com.celements.common.test.AbstractComponentTest;
+import com.celements.model.object.xwiki.XWikiObjectFetcher;
+import com.celements.model.reference.RefBuilder;
+import com.celements.web.classes.oldcore.XWikiGroupsClass;
+import com.xpn.xwiki.XWikiConstant;
+import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.objects.classes.BaseClass;
+
+public class XWikiAdminUserTest extends AbstractComponentTest {
+
+  private static final String CFG_KEY_ENABLED = "celements.mandatory.enabled.XWikiAdminUser";
+
+  private XWikiAdminUser mandatory;
+
+  @Before
+  public void prepareTest() throws Exception {
+    mandatory = getBeanFactory().getBean("celements.mandatory.XWikiAdminUser",
+        XWikiAdminUser.class);
+  }
+
+  @Test
+  public void testDependsOnMandatoryDocuments() throws Exception {
+    assertEquals(2, mandatory.dependsOnMandatoryDocuments().size());
+    assertEquals("celements.MandatoryGroups", mandatory.dependsOnMandatoryDocuments().get(0));
+    assertEquals(MandatoryDiskDocuments.class.getName(), mandatory.dependsOnMandatoryDocuments().get(
+        1));
+  }
+
+  @Test
+  public void testIsEnabled_default() throws Exception {
+    getXContext().setDatabase("xwiki");
+
+    replayDefault();
+    assertFalse(mandatory.isEnabledByDefault());
+    assertFalse(mandatory.isEnabled());
+    verifyDefault();
+  }
+
+  @Test
+  public void testIsEnabled_configured() throws Exception {
+    getXContext().setDatabase("xwiki");
+    getConfigurationSource().setProperty(CFG_KEY_ENABLED, "1");
+
+    replayDefault();
+    assertTrue(mandatory.isEnabled());
+    verifyDefault();
+  }
+
+  @Test
+  public void testSkip_mainWiki() throws Exception {
+    getXContext().setDatabase("xwiki");
+
+    replayDefault();
+    assertFalse(mandatory.skip());
+    verifyDefault();
+  }
+
+  @Test
+  public void testSkip_localWiki() throws Exception {
+    getXContext().setDatabase("subwiki");
+
+    replayDefault();
+    assertTrue(mandatory.skip());
+    verifyDefault();
+  }
+
+  @Test
+  public void testAddAdminUserToAdminGroup_create() throws Exception {
+    getXContext().setDatabase("xwiki");
+    DocumentReference groupRef = getAdminGroupRef();
+    XWikiDocument groupDoc = new XWikiDocument(groupRef);
+    BaseClass baseClass = expectNewBaseObject(XWikiGroupsClass.CLASS_REF.getDocRef(
+        groupRef.getWikiReference()));
+    expect(baseClass.get(XWikiGroupsClass.FIELD_MEMBER.getName()))
+        .andReturn(XWikiGroupsClass.FIELD_MEMBER.getXField())
+        .anyTimes();
+
+    replayDefault();
+    assertTrue(mandatory.addAdminUserToAdminGroup(groupDoc));
+    verifyDefault();
+
+    assertEquals(1, XWikiObjectFetcher.on(groupDoc).filter(XWikiGroupsClass.CLASS_REF).count());
+    assertEquals("XWiki." + XWikiAdminUser.ADMIN_DOC_NAME,
+        XWikiObjectFetcher.on(groupDoc)
+            .filter(XWikiGroupsClass.CLASS_REF)
+            .stream()
+            .findFirst()
+            .orElseThrow()
+            .getStringValue(XWikiGroupsClass.FIELD_MEMBER.getName()));
+  }
+
+  @Test
+  public void testAddAdminUserToAdminGroup_exists() throws Exception {
+    getXContext().setDatabase("xwiki");
+    DocumentReference groupRef = getAdminGroupRef();
+    XWikiDocument groupDoc = new XWikiDocument(groupRef);
+    BaseClass baseClass = expectNewBaseObject(XWikiGroupsClass.CLASS_REF.getDocRef(
+        groupRef.getWikiReference()));
+    expect(baseClass.get(XWikiGroupsClass.FIELD_MEMBER.getName()))
+        .andReturn(XWikiGroupsClass.FIELD_MEMBER.getXField())
+        .anyTimes();
+
+    replayDefault();
+    assertTrue(mandatory.addAdminUserToAdminGroup(groupDoc));
+    assertFalse(mandatory.addAdminUserToAdminGroup(groupDoc));
+    verifyDefault();
+
+    assertEquals(1, XWikiObjectFetcher.on(groupDoc).filter(XWikiGroupsClass.CLASS_REF).count());
+  }
+
+  private DocumentReference getAdminGroupRef() {
+    return new RefBuilder().wiki("xwiki")
+        .space(XWikiConstant.XWIKI_SPACE)
+        .doc("XWikiAdminGroup")
+        .build(DocumentReference.class);
+  }
+
+}
